825914e5c29fd5d14b8c1f2faf8d8123528ebb60
lejna <lejna@DESKTOP-64IL438>
2024-02-01 12:34:26 +0100

    Add project files.

---
+++ PJIspit/DataDisplay.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PJIspit
+{
+    public partial class DataDisplay : Form
+    {
+        public DataDisplay()
+        {
+            InitializeComponent();
+            try
+            {
+                FetchData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error fetching data: " + ex.Message);
+            }
+        }
+
+
+     private void FetchData()
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-64IL438\DEVSQL;Initial Catalog=PJstudents;Integrated Security=True");
+            con.Open();
+            string query = "SELECT * FROM students";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, con))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            con.Close();
+
+        }
+
+
+    private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PJIspit/InsertData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PJIspit
+{
+    public partial class InsertData : Form
+    {
+        public InsertData()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-64IL438\DEVSQL;Initial Catalog=PJstudents;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("insert into students values('" +
+                    textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "')", con);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Inserted Successfully.");
+                con.Close();
+                OpenDataDisplayForm();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inserting data: " + ex.Message);
+            }
+        }
+
+        private void OpenDataDisplayForm()
+        {
+            DataDisplay dataDisplayForm = new DataDisplay();
+
+            dataDisplayForm.Show();
+        }
+    }
+}
